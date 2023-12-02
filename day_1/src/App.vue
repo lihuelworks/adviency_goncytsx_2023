4109d6dbfebd28f71e7bed1281a7db24349df22b
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-12-01 21:07:39 -0300

    day 1 💪🏻

---
+++ day_1/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+let lista_regalos = ["🧦 Medias", "🍬 Caramelos", "🐟 Vitel Tone"]
+</script>
+
+<template>
+  <div class="app">
+    <h1>Regalos:</h1>
+    <ul>
+      <li v-for="item in lista_regalos">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  height: 80vh;
+  display: grid;
+  /* place-items: center; */
+  place-content: center;
+  row-gap: 1.5rem;
+
+}
+.app > ul {
+  justify-self: start;
+  list-style: none;
+  padding: 0;
+}
+
+</style>
